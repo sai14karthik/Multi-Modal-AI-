649d3049445d3e9fbaef263a00a46f64c95c74c0
Sai Karthik <sai14karthik@gmail.com>
2025-12-15 20:05:47 -0500

    Update run.sh with additional test command and improve .gitignore

--- run.sh
+++ run.sh
@@ -12,7 +12,6 @@ python -m src.main \
 
 
 
-
     python -m src.main \
     --data_root data \
     --modalities CT PET \
@@ -25,3 +24,16 @@ python -m src.main \
     --temperature 0.8 \
     --max_samples 2000 \
     --split val
+
+
+   python -m src.main \
+    --data_root data \
+    --modalities CT PET \
+    --model_name  openai_clip-vit-base-patch32\
+    --dataset_config data/dataset_config.yaml \
+    --class_names high_grade low_grade \
+    --batch_size 1 \
+    --temperature 0.5 \
+    --aggressive_preprocess \
+    --max_samples 15 \
+    --output_dir result
